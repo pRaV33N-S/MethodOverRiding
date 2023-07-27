045ed66a91a9e1ce1d01b392ed6bd1b3587894d0
pRaV33N-S <praveen.srinivasan1@mphasis.com>
2023-07-27 10:50:33 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOverRiding
+{
+    public class Employee
+    {
+        int id;
+        string name;
+        double sal;
+        public void Register()
+        {
+            Console.WriteLine("Enter Id");
+            id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Name");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter Salary");
+            sal = double.Parse(Console.ReadLine());
+        }
+        public void Display()
+        {
+            Console.WriteLine($"ID : {id}\nName : {name}\nSalary : {sal}");
+        }
+    }
+    public class Developer : Employee
+    {
+        string domain;
+        string projects;
+        public void Register()
+        {
+            //base.Register();
+            Console.WriteLine("Enter Domain");
+            domain = Console.ReadLine();
+            Console.WriteLine("Enter Project");
+            projects = Console.ReadLine();
+        }
+        public void Display()
+        {
+            base.Display();
+            Console.WriteLine($"Domain : {domain}\nProjects : {projects}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee emp = new Employee();
+            //emp.Register();
+            //emp.Display();
+            Developer dev = new Developer();
+            dev.Register();
+            dev.Display();
+        }
+    }
+}
